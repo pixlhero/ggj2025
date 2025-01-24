4ab63e74bdd598827e0d516e9bde9a48c9d689a3
Luca Tescari <luca@ltescari.ch>
2025-01-24 20:40:38 +0100

    first working version of first person controller

---
+++ NormalTrainGame/Assets/Scripts/FPC/PlayerCamera.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX = 100.0f;
+    public float sensY = 100.0f;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        
+    }
+}
