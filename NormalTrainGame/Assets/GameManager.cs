33b291c00fedc089b9420223eff30835125ed043
Luca Tescari <luca@ltescari.ch>
2025-01-25 09:29:34 +0100

    fixed gamemanager stuff

--- NormalTrainGame/Assets/GameManager.cs
+++ NormalTrainGame/Assets/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     // A singleton instance to allow easy access from any script
     public static GameManager Instance;
+
+    public int score;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
