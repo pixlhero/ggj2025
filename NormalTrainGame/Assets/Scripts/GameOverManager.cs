03842dddfa2b61c07b0d37c46c7bb210f62d3599
Luca Tescari <luca@ltescari.ch>
2025-01-25 16:52:24 +0100

    added gameovermanager

---
+++ NormalTrainGame/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    // A singleton instance to allow easy access from any script
+    public static GameOverManager Instance;
+
+    private void Awake()
+    {
+        // Enforce singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        // Make sure the AudioManager persists across scene loads
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
