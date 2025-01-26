9dcd4ad95bfc09d0981d4a32870b046213d1f772
Raphael Anderegg <raphael.anderegg@bitforge.ch>
2025-01-26 11:24:50 +0100

    fixed prefab kerfuffeel

--- NormalTrainGame/Assets/Scripts/GameloopManager.cs
+++ NormalTrainGame/Assets/Scripts/GameloopManager.cs
@@ -28,6 +28,7 @@ public class GameloopManager : MonoBehaviour
     public int score;
     
     private int[] maxBabies = new int[]{6, 11, 20, 31};
+    private float [] timePerBaby = new float[]{15, 10, 7, 4};
     
     private int _currentLevel = 0;
     
@@ -37,7 +38,7 @@ public class GameloopManager : MonoBehaviour
     
     private bool _isInCooldownMode = false;
     
-    private float maxTime = 15f;
+    private float maxTime => timePerBaby[_currentLevel];
 
     private void Awake()
     {
